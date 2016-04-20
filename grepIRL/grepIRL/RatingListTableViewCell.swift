ae2f9f02faa7342ca9c3ef2c87daab2858e51959
George He <georgehe@berkeley.edu>
2016-04-20 16:41:06 -0700

    Initial files created

---
+++ grepIRL/grepIRL/RatingListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RatingListTableViewCell.swift
+//  grepIRL
+//
+//  Created by George He on 4/20/16.
+//  Copyright © 2016 George He. All rights reserved.
+//
+
+import UIKit
+
+class RatingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ grepIRL/grepIRL/TrackedItem.swift
@@ -0,0 +1,9 @@
+//
+//  TrackedItem.swift
+//  grepIRL
+//
+//  Created by George He on 4/20/16.
+//  Copyright © 2016 George He. All rights reserved.
+//
+
+import Foundation
